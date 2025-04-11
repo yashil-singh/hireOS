00992c17d08a17fc7e375c734bebba0d2da2f184
Yashil Lal Singh <yashilsingh0208@gmail.com>
2025-04-12 02:00:42 +0545

    Project Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/assets/logo.png";
+
+function App() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <img src={Logo} className="mx-auto h-14" />
+    </div>
+  );
+}
+
+export default App;
